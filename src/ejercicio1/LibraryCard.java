da64de86023fb9588042e20988aa8cb7734defa3
Estudiante <Estudiante@10.203.2.38>
2017-08-24 15:32:42 -0500

    Prog Clase

---
+++ src/ejercicio1/LibraryCard.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+import java.util.*;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class LibraryCard {
+    public Student name;
+    String Owner;
+    int BorrowCnt;
+    
+    public void LibraryCard(){}
+    public void checkout(){
+    BorrowCnt = BorrowCnt + 1;
+    }
+    public void getNumberOfBooks(){}
+    public void getOwnerName(){
+    Owner = Student.name ;}
+    public void setOwner(Student);{
+   
+    }   
+   // public toString(){}
+           
+    
+}
---
+++ src/ejercicio1/Student.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+import java.util.*;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Student {
+    String name;
+    String email;
+    public void getEmail(){
+    Scanner getEmail = new Scanner(System.in);
+    email = getEmail.nextLine();
+  
+   
+    }
+    public void getName(){
+    Scanner getName = new Scanner(System.in);
+    name = getName.nextLine();    
+    }
+    public void setEmail(){
+        
+        
+                   
+    }
+    public void setName(){
+       
+    }
+    
+}
